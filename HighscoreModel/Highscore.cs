4f567c6be9bbbfc0f95a01d38e86b46dbc832c9a
Michael-MO <Michael-MO@users.noreply.github.com>
2018-10-23 18:00:18 +0200

    Initial commit

---
+++ HighscoreModel/Highscore.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HighscoreModel
+{
+    public class Highscore
+    {
+        private int _id;
+        private string _name;
+        private int _score;
+
+        public Highscore() { }
+
+        public Highscore(int id, string name, int score)
+        {
+            _id = id;
+            _name = name;
+            _score = score;
+        }
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value;  }
+        }
+
+        public int Score
+        {
+            get { return _score; }
+            set { _score = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}: {Score}";
+        }
+    }
+}
---
+++ HighscoreRest/Controllers/HighscoresController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HighscoreModel;
+
+namespace HighscoreRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HighscoresController : ControllerBase
+    {
+        private static List<Highscore> highscoreList = new List<Highscore>();
+
+        // GET: api/Highscores
+        [HttpGet]
+        public IEnumerable<Highscore> Get()
+        {
+            return highscoreList;
+        }
+
+        // POST: api/Highscores
+        [HttpPost]
+        public void Post([FromBody] Highscore obj)
+        {
+            obj.ID = highscoreList.Count;
+            highscoreList.Add(obj);
+            highscoreList.OrderByDescending(o => o.Score).ToList();
+        }
+
+        // PUT: api/Highscores/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
